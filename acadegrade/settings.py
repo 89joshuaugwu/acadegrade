798f063c912a3db703f1e4f16cd0d52b4deb50f0
89joshuaugwu <joshuaugwu89@gmail.com>
2025-09-21 00:25:47 +0100

    Initial commit for Render deployment

--- acadegrade/settings.py
+++ acadegrade/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-jq(!^$j0-2mm27#&g8m7v9av2bc=lpgovk#iw+qlgqh6*lwd(#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["acadegrade.onrender.com"]
 
 
 # Application definition
